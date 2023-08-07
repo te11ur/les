eaedcce9ab50746889bf1a14e44ad9e12532fdf9
Reshad Gulam <r,gulam@freeplay.io>
2023-08-07 21:26:54 +0300

    task1 - start task

--- src/task1/resgul/index.js
+++ src/task1/resgul/index.js
@@ -1,3 +1,44 @@
-// ЧТО!? ПОДГЛЯДЫВАЕМ >_< !?
-document.body.style.backgroundColor = '#008080';
-console.log(123);
+import {newID} from './newID.js'
+
+const Nodes = new Map();
+
+// {id, next,prev,children,parent}
+class Node {
+  id = 0;
+  parent = null;
+  children = new Map();
+
+  constructor() {
+    this.id = newID();
+
+    Nodes.set(this.id, this);
+  };
+
+  add(node) {
+    this.children.set(node.id, node);
+  };
+
+  remove(id) {
+    this.children.delete(id);
+  };
+
+  next() {
+
+  };
+
+  prev() {
+
+  };
+
+  eachChild(cb) {
+    for (let i = 0; i < this.children.size - 1; i++) {
+      
+
+    };
+  };
+
+};
+
+const node1 = new Node();
+
+console.log(Nodes);
