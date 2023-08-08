7090f0f28c922a822c7a187450c7aa8b241f3dc4
Alexei <a.kravchuk@freeplay.by>
2023-08-08 22:10:37 +0200

    task1 swap

--- src/task1/kravchuk/Node.js
+++ src/task1/kravchuk/Node.js
@@ -30,8 +30,14 @@ export class Node {
     });
   }
 
+  /**
+   * Delete self
+   * @returns {Node} self node
+   */
   removeSelf() {
     const { next, prev, parent } = this;
+
+    this.parent = this.next = this.prev = null;
 
     if (!prev) {
       // этот первый — следующий становится первым
@@ -58,31 +64,54 @@ export class Node {
   /**
    *
    * @param {number|Node} child - child id or child itself
+   * @returns {Node} removed child
    */
   removeChild(child) {
-    if (!(child instanceof Node)) throw Error('child should be instanceof Node');
+    if (!(child instanceof Node)) {
+      throw Error('child should be instanceof Node');
+    }
 
     if (isNumber(child)) {
-      let targetNode;
+      let next = children;
+      do {
+        if (next.id === child) {
+          return next.removeSelf();
+          break;
+        }
+        next = next.next;
+      } while (next);
     } else {
-      child.removeSelf();
+      return child.removeSelf();
     }
   }
 
+  /**
+   * @param {Function} fn - handler function
+   */
   eachSibling(fn) {
-    if (!isFunction(fn)) throw Error('argument must be a function');
+    if (!isFunction(fn)) {
+      throw Error('argument must be a function');
+    }
 
     const first = getFirstSibling(this);
 
     eachNode(first, fn);
   }
 
+  /**
+   * @param {Function} fn - handler function
+   */
   eachChild(fn) {
-    if (!isFunction(fn)) throw Error('argument must be a function');
+    if (!isFunction(fn)) {
+      throw Error('argument must be a function');
+    }
 
     eachNode(this.children, fn);
   }
 
+  /**
+   * Revers childrens chain
+   */
   reverseChildren() {
     let current = this.children;
     let next, prev;
@@ -101,7 +130,54 @@ export class Node {
 
     this.children = current;
   }
+
+  /**
+   * Swap any nodes
+   * @param {Node} node1
+   * @param {Node} node2
+   */
+  swap(node1, node2) {
+    if (!(node1 instanceof Node && node1 instanceof Node)) {
+      throw Error('both arguments should be instanceof Node');
+    }
+
+    swapNodes(node1, node2);
+  }
+
+  swapSelf(node) {
+    swapNodes(this, node);
+  }
 }
+
+const swapNodes = (node1, node2) => {
+  const parent1 = node1.parent;
+  const parent2 = node2.parent;
+  if (parent1) parent1.children = node2;
+  if (parent2) parent2.children = node1;
+  node1.parent = parent2;
+  node2.parent = parent1;
+
+  const children1 = node1.children;
+  const children2 = node2.children;
+  if (children1) children1.parent = node2;
+  if (children2) children2.parent = node1;
+  node2.children = children1;
+  node1.children = children2;
+
+  const next1 = node1.next;
+  const next2 = node2.next;
+  if (next1) next1.prev = node2;
+  if (next2) next2.prev = node1;
+  node2.next = next1;
+  node1.next = next2;
+
+  const prev1 = node1.prev;
+  const prev2 = node2.prev;
+  if (prev1) prev1.next = node2;
+  if (prev2) prev2.next = node1;
+  node1.prev = prev2;
+  node2.prev = prev1;
+};
 
 const eachNode = (firstNode, fn) => {
   let next = firstNode;
@@ -117,6 +193,7 @@ const getFirstSibling = (node) => {
   if (node.parent) {
     first = node.parent.children;
   } else {
+    first = node;
     while (first.prev) {
       first = first.prev;
     }
--- src/task1/kravchuk/index.js
+++ src/task1/kravchuk/index.js
@@ -5,13 +5,35 @@ import { Node } from './Node.js';
 
 const rootNode = new Node('root');
 
-const l1Child1 = new Node('level1_1');
-const l1Child2 = new Node('level1_2');
-const l1Child3 = new Node('level1_3');
-const l1Child4 = new Node('level1_4');
-const l1Child5 = new Node('level1_5');
+const child_11 = new Node('child_11');
+const child_12 = new Node('level_12');
+const child_13 = new Node('level_13');
+const child_14 = new Node('level_14');
+const child_15 = new Node('level_15');
 
-rootNode.add(l1Child1, l1Child2, l1Child3, l1Child4, l1Child5);
+const child_21 = new Node('level_21');
+const child_22 = new Node('level_22');
+const child_23 = new Node('level_23');
+const child_24 = new Node('level_24');
+const child_25 = new Node('level_25');
+
+rootNode.add(child_11, child_12, child_13, child_14, child_15);
+
+child_11.add(child_21);
+child_12.add(child_22);
+child_22.add(child_23);
+child_23.add(child_24);
+child_13.add(child_25);
+
+// child_23.removeSelf();
+// console.log(child_23);
+
+/* child_23.swapSelf(child_13);
+console.log(child_23);
+console.log(child_23.children === child_25);
+console.log(child_13.children === child_24);
+console.log(child_24.parent === child_13);
+console.log(child_25.parent === child_23); */
 
 // l1Child5.removeSelf();
 // console.log(l1Child5);
@@ -22,8 +44,8 @@ rootNode.add(l1Child1, l1Child2, l1Child3, l1Child4, l1Child5);
 // rootNode.eachChild((child) => console.log(child.value));
 // l1Child1.eachSibling((sibling) => console.log(sibling.value));
 
-rootNode.eachChild((child) => console.log(child.value));
+/* rootNode.eachChild((child) => console.log(child.value));
 rootNode.reverseChildren();
-console.log('-\\\\-//-\\\\-//-');
+console.log('--//--');
 rootNode.eachChild((child) => console.log(child.value));
-console.log(rootNode.children);
+console.log(rootNode.children); */
