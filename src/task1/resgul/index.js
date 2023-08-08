e79f5f136082a9be8e5e4d8173260a1425205a0a
Reshad Gulam <r,gulam@freeplay.io>
2023-08-09 01:52:27 +0300

    task1 - reworked with Array

--- src/task1/resgul/index.js
+++ src/task1/resgul/index.js
@@ -1,92 +1,105 @@
 import {newID} from './newID.js'
 
-const Nodes = new Map();
+const WRAPPER = [];
+WRAPPER.name = 'WRAPPER';
 
 // {id, next,prev,children,parent}
 class Node {
   id = 0;
   parent = null;
-  children = new Map();
-  name = '';
+  children = [];
 
   constructor(parent) {
     this.id = newID();
-    this.name = `нод с id = ${this.id}`;
-
-    if (parent) this.parent = parent;
-    else this.parent = Nodes;
-
-    Nodes.set(this.id, this);
+    this.name = `node_` + this.id;
+    // определение родителя
+    if (parent) {
+      this.parent = parent;
+      parent.add(this);
+    } else {
+      this.parent = WRAPPER;
+      this.parent.push(this);
+    };
   };
 
   add(nodes) {
     // возможность добавлять несколько нод за раз
     for (let i = 0; i < arguments.length; i++) {
       const node = arguments[i];
-      // добавления текущего нода в коллекцию
-      this.children.set(node.id, node);
-      // удаление предыдущего родителя, если есть
-      if (node.parent.remove) node.parent.remove(node);
-      if (node.parent.delete) node.parent.delete(node.id);
-      // назначение нового родителя
+      if (this === node) {
+        console.error('попытка добавить node в самого себя');
+        break;
+      }
+      
+      // удаление из предыдущего родителя
+      if (Array.isArray(node.parent)) {
+        const removeNode = this.remove.bind(WRAPPER);
+        removeNode(node, false);
+      } else node.parent.remove(node, false);
+
+      // назначение нового родителя и добавляю в новых чилдов
       node.parent = this;
+      this.children.push(node);
     };
   };
 
-  remove(node) {
-    this.children.delete(node.id);
+  remove(node, completely = true) {
+    const removeCompletely = completely;
+    const children = (Array.isArray(this)) ? this : this.children;
+    const index = children.indexOf(node);
+
+    if (index === -1) {
+      console.error('попытка удалить node, которго нет в чилдренах');
+      return;
+    }
+    // удаление элемента из чилдов текщего перента
+    children.splice(index, 1);
+    // если полное удаление, а не перемещение
+    if (removeCompletely) {
+      for (let prop in node) delete node[prop];
+    };
   };
 
   get next() {
-    const mapWithNeighbors = this.parent.children;
-    const keys = mapWithNeighbors.keys();
-    return keys.next();
+    const children = Array.isArray(this.parent) ? this.parent : this.parent.children;
+    const index = children.indexOf(this);
+    const nextElement = (index > -1 && children[index + 1]) ? 
+    children[index + 1] : null;
+    return nextElement;
   };
 
-  prev() {
-
+  get prev() {
+    const children = Array.isArray(this.parent) ? this.parent : this.parent.children;
+    const index = children.indexOf(this);
+    const prevElement = (index > -1 && children[index - 1]) ? 
+    children[index - 1] : null;
+    return prevElement;
   };
 
-  eachChild(cb) {
-    let i = 0;
-    const keys = this.children.keys();
-
-    for (let key of keys) {
-      const child = this.children.get(key);
-      cb?.(child, i++);
+  each(cb) {
+    for (let i = 0; i < this.children.length; i++) {
+      const child = this.children[i];
+      cb?.(child, i);
     };
   };
 };
 
-// тест
+// debbug
+const node0 = new Node();
 const node1 = new Node();
-
-const nodeChild1 = new Node();
-const nodeChild2 = new Node();
-
 const node2 = new Node();
 const node3 = new Node();
 const node4 = new Node();
-const node5 = new Node();
-const node6 = new Node();
-const node7 = new Node();
+// выдаст ошибку из-за node4
+node4.add(node0, node3, node2, node4);
+// выдаст ошибку из-за node1
+node0.remove(node1);
+node4.each((child, i) => {
+  console.log('each_test: ', child.name, i);
+})
 
-// добавить в node1 два чилда
-node1.add(nodeChild1, nodeChild2, node4);
-// забрать одного чилда в node3
-node3.add(nodeChild2);
-// добавить нод с собственными чилдами в другой нод
-node3.add(node1, nodeChild1, node2);
-// удалить чилда
-node3.remove(nodeChild2);
-// форыч
-node3.eachChild((child, i) => {
-  console.log(child, i)
-});
-
-
-console.log(Nodes);
-
-console.log(node4.next);
-console.log(node4.next);
-console.log(node4.next);
+console.log('node4_children: ', node4.children);
+console.log('prev_of_node3: ', node3.prev);
+console.log('next_of_node3: ', node3.next);
+console.log('next_of_node4: ', node4.next);
+console.log('wrapper_arr: ', WRAPPER);
