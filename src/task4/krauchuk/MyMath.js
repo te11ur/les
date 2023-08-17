ad7bdf03c47fe3c0ac509d3922ff4ffa9fc61162
Alexei <a.kravchuk@freeplay.by>
2023-08-17 10:35:15 +0200

    task4 krauchuk

---
+++ src/task4/krauchuk/MyMath.js
@@ -0,0 +1,76 @@
+import { isFunction } from '../../utils/types/isFunction.js';
+
+const locked = (_target, _key, descriptor) => ({
+  ...descriptor,
+  writable: false,
+});
+
+const toPromise = (_target, _name, descriptor) => {
+  const original = descriptor.value;
+  let value = original;
+
+  if (isFunction(original)) {
+    value = (...args) => Promise.resolve(original(...args));
+  }
+
+  return { ...descriptor, value };
+};
+
+export class MyMath {
+  @locked
+  static PI = 3.14159265359;
+  @locked
+  static E = 2.718281828459045;
+
+  @toPromise
+  static abs(n) {
+    return n > 0 ? n : n * -1;
+  }
+
+  @toPromise
+  static pow(a, b) {
+    return a ** b;
+  }
+
+  @toPromise
+  static sqrt(n) {
+    return n ** 0.5;
+  }
+
+  @toPromise
+  static sign(val) {
+    return val === 0 ? 0 : val > 0 ? 1 : -1;
+  }
+
+  @toPromise
+  static clamp(val, min, max) {
+    return Math.min(Math.max(val, min), max);
+  }
+
+  @toPromise
+  static withinRange(val, min, max) {
+    return val >= min && val <= max;
+  }
+
+  @toPromise
+  static lerp(a, b, alpha) {
+    return a + alpha * (b - a);
+  }
+
+  @toPromise
+  static degToRad(degrees) {
+    return degrees * (Math.PI / 180);
+  }
+
+  @toPromise
+  static radToDeg(radians) {
+    return radians * (180 / Math.PI);
+  }
+
+  @toPromise
+  static distance(x1, y1, x2, y2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return MyMath.sqrt(dx * dx + dy * dy);
+  }
+}
---
+++ src/task4/krauchuk/index.js
@@ -0,0 +1,7 @@
+import { MyMath } from './MyMath.js';
+
+console.log(MyMath.pow(2, 3));
+MyMath.pow(2, 3).then((res) => console.log(res));
+
+// MyMath.PI = 1;
+console.log(MyMath.PI);
