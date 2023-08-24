b7d8d8170029614a9bf3c486d9d532ae3d571dda
Mariya <m.lebadina@freeplay.io>
2023-08-24 15:57:40 +0300

    task6 lebadina delete worker.js

--- src/task6/lebadina/worker.js
+++
@@ -1,9 +0,0 @@
-// export const onmessage = async (e) => {
-//     const { method, args } = e.data;
-//     console.log('+++++')
-//     if (self[method]) {
-//         const result = await self[method](...args);
-//         postMessage(result);
-//     }
-// };
-
